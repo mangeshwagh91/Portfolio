ff5969dba19cfa50cbff72001332d7039dbba6ae
mangeshwagh91 <wmangesh91@gmail.com>
2026-07-03 21:27:37 +0530

    fix

--- src/components/GrainyNoise.jsx
+++
@@ -1,5 +0,0 @@
-export default function GrainyNoise() {
-  return (
-    <div className="pointer-events-none fixed inset-0 z-[9999] h-[400%] w-[400%] animate-noise bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-[0.05]" />
-  );
-}
--- src/components/theme-provider.jsx
+++
@@ -1,3 +0,0 @@
-export function ThemeProvider({ children, ...props }) {
-  return <>{children}</>
-}
